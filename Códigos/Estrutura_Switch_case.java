26f08c0354dae6c0504cbb4f74c22ffede598618
Matheus Henrique Pereira <mhp.jnd@gmail.com>
2025-08-26 20:57:49 -0300

    Update Estrutura_Switch_case

---
+++ Códigos/Estrutura_Switch_case.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Estrutura_Switch_case{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        String dia;
+
+        switch(x) {
+            case 1:
+                    dia = "Domingo";
+                    break;
+            case 2:
+                    dia = "Segunda";
+                    break;
+            case 3:
+                    dia = "Terça";
+                    break;
+            case 4: 
+                    dia = "Quarta";
+            case 5: 
+                    dia = "Quinta";
+                    break;
+            case 6: 
+                    dia = "Sexta";
+                    break;
+            case 7:
+                    dia = "Sabado";
+                    break;
+            default: 
+                    dia = "Valor invalido";
+                    break;
+        }
+
+        System.out.println("Dia da semana: " + dia);
+        sc.close();
+    }
+}
