7369eb254e790dee16276055f20878a644a3fc71
Matheus Henrique Pereira <mhp.jnd@gmail.com>
2025-10-14 20:37:50 -0300

    Matriz

--- Códigos/Exercicio_Matriz/Ex01.java
+++ Códigos/Exercicio_Matriz/Ex01.java
@@ -33,10 +33,23 @@ public class Ex01 {
             for(int j=0; j<mat[i].length; j++){
                 if(mat[i][j] == num){
                     System.out.println("Position "+i+","+j+": ");
+
+                    if(j>0){
+                        System.out.println("Left: " + mat[i][j-1]);
+                    }
+                    if(i>0){
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if(j < mat[i].length-1){
+                        System.out.println("Right: " + mat[i][j+1]);
+                    }
+                    if(i<mat.length-1){
+                        System.out.println("Down: " + mat[i+1][j]);
+                    }
                 }
             }
         }
-        
+        sc.close();
        
     }
 }
